fc6543dcf8cb4ae6e479aee4110d63881f1b3ea2
Bobobo <16814488+bobobo-git@users.noreply.github.com>
2025-10-06 05:54:01 +0200

    Add files via upload

--- docs/scripts/dub/dub_mute_5.lua
+++ docs/scripts/dub/dub_mute_5.lua
@@ -8,7 +8,7 @@ getnummidioutnumber()
 full_path = debug.getinfo(1, "S").source:sub(2)
 index = full_path:match(".*_(.-)%.lua$")
 t=index-1
-
+led=1+t*3
 track = reaper.GetTrack(0, t)
 
 id=({reaper.get_action_context()})[4]
@@ -17,12 +17,12 @@ onoff= reaper.GetMediaTrackInfo_Value(track,"B_MUTE")
 if onoff == 0 then
  reaper.SetMediaTrackInfo_Value(track, "B_MUTE",1)
  reaper.SetToggleCommandState(1,id,1)
- reaper.StuffMIDIMessage(mout,0x90 ,13, 0x7F)
+ reaper.StuffMIDIMessage(mout,0x90 ,led, 0x7F)
  
 else
  reaper.SetMediaTrackInfo_Value(track, "B_MUTE",0)
  reaper.SetToggleCommandState(1,id,0)
- reaper.StuffMIDIMessage(mout,0x90 ,13, 0x00)
+ reaper.StuffMIDIMessage(mout,0x90 ,led, 0x00)
 end
 
 
