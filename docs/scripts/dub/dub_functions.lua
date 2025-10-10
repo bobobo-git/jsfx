74843ab72d0d64c8e8b4ad77fdc263d4dc27ba29
Bobobo <16814488+bobobo-git@users.noreply.github.com>
2025-10-11 00:43:27 +0200

    Add files via upload

--- docs/scripts/dub/dub_functions.lua
+++ docs/scripts/dub/dub_functions.lua
@@ -9,6 +9,37 @@ while i<ii do
   end
   return mout
 end
+
+function blink()
+
+  i=0 -- 
+  function wait(mseconds)
+    local start = os.clock()
+    m1=start
+    m2=start+mseconds
+    repeat until os.clock() > start + mseconds
+  end
+
+    for i = 1,  8 do
+      led=3*i
+      reaper.StuffMIDIMessage(mout,0x90 ,led, 0x7F)
+      reaper.StuffMIDIMessage(mout,0x90 ,led-2, 0x7F)
+      wait(0.05)
+      reaper.StuffMIDIMessage(mout,0x90 ,led, 0x00)
+      reaper.StuffMIDIMessage(mout,0x90 ,led-2, 0x00)
+    end
+
+    for i = 1,  8 do
+      led=(8 - math.tointeger(i)+1)*3
+      reaper.StuffMIDIMessage(mout,0x90 ,led, 0x7F)
+      reaper.StuffMIDIMessage(mout,0x90 ,led-2, 0x7F)
+      wait(0.05)
+      reaper.StuffMIDIMessage(mout,0x90 ,led, 0x00)
+      reaper.StuffMIDIMessage(mout,0x90 ,led-2, 0x00)
+
+    end
+end
+
 getnummidioutnumber()
 
 if reaper.GetExtState("recarms","Version")=='' then  -- wird aus ExtState geladen, wenn nichts da ist wird 9 gesetzt
